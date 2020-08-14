dff619490bcb77b6358b5e985511c9ab91ea8f62
jripke74 <jripke74@gmail.com>
2020-08-14 12:11:38 -0700

    add req.on() method to read data

--- app.js
+++ app.js
@@ -1,17 +1,32 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
   const url = req.url;
+  const method = req.method;
   if (url === '/jeff') {
-    res.write('<head><title>Enter a Message</title></head>');
-    res.write('<h1>Hello There Jeff!!!</h1>');
-    res.write('<form action="/message" method="POST"><input type="text" name="message"><button type="submit">Submit</button></form>');
-    return res.end();
-  } else if (url === '/message') {
-    res.write('<h1>Message Sent!!!');
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<form method="POST" action="/message"><input type="text" name="message"><button type="submit">Submit</button></form>');
     return res.end();
   }
-  res.write('<a href="/jeff">Send a message</a>');
+  if (url === '/message' && method === 'POST') {
+    const body = [];
+    req.on('data', (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    req.on('end', () => {
+      const parseBody = Buffer.concat(body).toString();
+      // const message = parseBody.split('=')[1];
+      fs.writeFileSync('message.txt', parseBody);
+    });
+    res.statusCode = 302;
+    res.setHeader('Location', '/');
+    return res.end();
+  }
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<a href="/jeff">Send Message</a>');
+  res.end();
 });
 
 server.listen(3000);
