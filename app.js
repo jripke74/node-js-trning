b040c124fa3efb52abfeb21e9ebdf9fe53d2357e
jripke74 <jripke74@gmail.com>
2020-08-14 09:39:11 -0700

    add form and message functionality

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  console.log(req);
+  const url = req.url;
+  if (url === '/jeff') {
+    res.write('<head><title>Enter a Message</title></head>');
+    res.write('<h1>Hello There Jeff!!!</h1>');
+    res.write('<form action="/message" method="POST"><input type="text" name="message"><button type="submit">Submit</button></form>');
+    return res.end();
+  } else if (url === '/message') {
+    res.write('<h1>Message Sent!!!');
+    return res.end();
+  }
+  res.write('<a href="/jeff">Send a message</a>');
 });
 
 server.listen(3000);
