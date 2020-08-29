524be6f17ede9279cc82b9752cff33d2098056ee
jripke74 <jripke74@gmail.com>
2020-08-29 11:32:34 -0700

    08 retrieving data

--- util/database.js
+++ util/database.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-  host: 'localhost',
+  host: '127.0.0.1',
   user: 'root',
   database: 'node-complete',
   password: 'ljljlkjidngk*@*KKJdlIA'
