ece9b181e5339c8d076e5aa460adb955b4c1644b
jripke74 <jripke74@gmail.com>
2020-08-31 11:41:24 -0700

    08 where conditions getting a single product

--- controllers/shop.js
+++ controllers/shop.js
@@ -16,16 +16,16 @@ exports.getProducts = (req, res, next) => {
 };
 
 exports.getProduct = (req, res, next) => {
-  const prodId = req.params.productId;
-  Product.findById(prodId)
-    .then(([product]) => {
-      res.render('shop/product-detail', {
-        product: product[0],
-        pageTitle: product.title,
-        path: '/products'
-      });
-    })
-    .catch(err => console.log(err));
+ const prodId = req.params.productId;
+ Product.findByPk(prodId)
+  .then((product) => {
+    res.render('shop/product-detail', {
+      product: product,
+      pageTitle: product.title,
+      path: '/products'
+    });
+  })
+  .catch(err => console.log(err));
 }
 
 exports.getIndex = (req, res, next) => {
