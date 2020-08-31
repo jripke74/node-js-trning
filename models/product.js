d47fd623aae5411d6098eba31eb8001a2a8b9b03
jripke74 <jripke74@gmail.com>
2020-08-31 10:19:04 -0700

    06 inserting data creating a product

--- models/product.js
+++ models/product.js
@@ -5,7 +5,7 @@ const sequelize = require('../util/database');
 const Product = sequelize.define('products', {
   id: {
     type: Sequelize.INTEGER,
-    autoIcrement: true,
+    autoIncrement: true,
     allowNull: false,
     primaryKey: true
   },
