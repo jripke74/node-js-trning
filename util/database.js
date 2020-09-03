60d0432e87150e7fcfdfeca1391ffcfa19fe855a
jripke74 <jripke74@gmail.com>
2020-09-03 15:43:28 -0700

    7 finishing the database connection

--- util/database.js
+++ util/database.js
@@ -1,15 +1,27 @@
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
+let _db;
+
 const mongoConnect = (callback) => {
-  MongoClient.connect('mongodb+srv://root:ipyzdk7jjjc82e9xqpd5tiv98c7czjs2g@cluster0.euzzj.mongodb.net/test?retryWrites=true&w=majority')
+  MongoClient.connect('mongodb+srv://root:ipyzdk7jjjc82e9xqpd5tiv98c7czjs2g@cluster0.euzzj.mongodb.net/shop2?retryWrites=true&w=majority')
   .then(client => {
     console.log('Connected!');
-    callback(client);
+    _db = client.db();
+    callback();
   })
   .catch(err => {
     console.log(err);
+    throw err;
   });
 }
 
-module.exports = mongoConnect;
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw 'No database found!';
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
