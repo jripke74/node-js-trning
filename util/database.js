457636a897f5d1efcd9e5fe97cecb955ebb1debb
jripke74 <jripke74@gmail.com>
2020-09-09 15:37:41 -0700

    05 installing the mongodb driver

--- util/database.js
+++ util/database.js
@@ -2,14 +2,16 @@ const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
 const mongoConnect = (callback) => {
-  MongoClient.connect('mongodb+srv://root:ipyzdk7jjjc82e9xqpd5tiv98c7czjs2g@cluster0.euzzj.mongodb.net/test?retryWrites=true&w=majority')
-  .then(client => {
-    console.log('Connected!');
-    callback(client);
-  })
-  .catch(err => {
-    console.log(err);
-  });
-}
+  MongoClient.connect(
+    'mongodb+srv://root:rp2er8srsz3rfjh2d493a32fj@cluster0.euzzj.mongodb.net/test?retryWrites=true&w=majority'
+  )
+    .then((client) => {
+      console.log('Connected!');
+      callback(client);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
 
 module.exports = mongoConnect;
