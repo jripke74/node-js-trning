6d2331f6eed2cb1896df103166a2fe3b482b54ec
jripke74 <jripke74@gmail.com>
2020-09-26 10:43:26 -0700

    10 initializing the sessions middleware

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,16 +1,14 @@
 exports.getLogin = (req, res, next) => {
   const isLoggedIn = req
     .get('Cookie')
-    .split('=')[1];
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
     isAuthenticated: isLoggedIn
   });
-  console.log(isLoggedIn);
 };
 
 exports.postLogin = (req, res, next) => {
-  res.setHeader('Set-Cookie', 'loggedIn=true');
+  res.setHeader('Set-Cookie', 'loggedIn=true;');
   res.redirect('/');
 };
